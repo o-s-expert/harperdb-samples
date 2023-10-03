b2c3480dfd1f940c9ad40994d970d37e2c8db5a3
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-03 10:07:08 +0100

    chore: create initial project to harpderdb
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 01-hello-world/src/main/java/org/jnosql/demo/se/VillainService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+package org.jnosql.demo.se;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.nosql.keyvalue.KeyValueTemplate;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class VillainService {
+
+
+    @Inject
+    private KeyValueTemplate template;
+
+
+    public void put(Villain villain) {
+        template.put(villain);
+    }
+
+    public Optional<Villain> get(String name) {
+        return template.get(name, Villain.class);
+    }
+
+}
