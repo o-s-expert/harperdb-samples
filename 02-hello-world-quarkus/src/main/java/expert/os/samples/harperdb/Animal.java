d1e69c4d6f317307191b1e6163e3f00fb0505169
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-26 17:30:28 +0100

    feat: create factory method to Animal
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 02-hello-world-quarkus/src/main/java/expert/os/samples/harperdb/Animal.java
+++ 02-hello-world-quarkus/src/main/java/expert/os/samples/harperdb/Animal.java
@@ -1,6 +1,14 @@
 package expert.os.samples.harperdb;
 
+import net.datafaker.Faker;
+
 public record Animal(String scientificName, String name, String genus, String species) {
 
 
+    public static Animal of(Faker faker){
+        var animal = faker.animal();
+        return new Animal(animal.scientificName(),
+                animal.name(),
+                animal.genus(), animal.species());
+    }
 }
