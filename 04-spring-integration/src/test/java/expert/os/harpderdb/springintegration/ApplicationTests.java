ee46918928d31135a7bcb5a819e816dbb34fd87b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-12-06 17:14:15 +0000

    fix: integration test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 04-spring-integration/src/test/java/expert/os/harpderdb/springintegration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.os.harpderdb.springintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
